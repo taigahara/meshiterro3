bb17096a84285ad72c2ca03bfd528c7ac206b361
原　大河 <tigerhara@yahoo.ne.jp>
2020-09-30 21:01:04 +0900

    depuroiganbaru

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c342e686c3df9ecb775e181cea28278156daa4e67d698a92ea2a28d3ab1d2612b00edb6c6c740dff4e84cf618ba4a7d616b216c8951918dcc7ab2e600156a119'
